3b103eb0c696f3208ba545649c21ebf2d6a0d98f
raghulkrishnan <rbalasubramanian1@hawk.iit.edu>
2020-11-28 14:53:36 -0800

    Goals and Users class added.

---
+++ app/src/main/java/com/example/activitytracker/model/User.java
@@ -0,0 +1,83 @@
+package com.example.activitytracker.model;
+
+public class User {
+    private int id;
+    private String fName;
+    private String lName;
+    private int age;
+    private String gender;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", Username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
