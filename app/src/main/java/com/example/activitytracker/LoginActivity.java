ce658dfcede80efb1b839b3dfe5f93a440b71860
raghulkrishnan <rbalasubramanian1@hawk.iit.edu>
2020-12-02 16:32:06 -0800

    Migrating to firebase. Login and welcome pages.

--- app/src/main/java/com/example/activitytracker/LoginActivity.java
+++ app/src/main/java/com/example/activitytracker/LoginActivity.java
@@ -3,65 +3,88 @@ package com.example.activitytracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
-    int invalidCount = 0;
-//    private SqlHelper db;
+
+    EditText loginEmail, loginPwd;
+    ProgressBar loginProgressBar;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-//        db = SqlHelper.getInstance(getApplicationContext());
+
+        loginEmail = (EditText) findViewById(R.id.loginEmail);
+        loginPwd = (EditText) findViewById(R.id.loginPwd);
+        loginProgressBar = (ProgressBar) findViewById(R.id.loginProgressBar);
+
+        mAuth = FirebaseAuth.getInstance();
     }
-    public void onRegisterBtnClick(View view){
-        Intent intent = new Intent(com.example.activitytracker.LoginActivity.this, RegisterActivity.class);
+
+    public void loginOnSignInClick(View view){
+
+        String email = loginEmail.getText().toString().trim();
+        String pwd = loginPwd.getText().toString().trim();
+
+        if(email.isEmpty()){
+            loginEmail.setError("Enter Email ID");
+            loginEmail.requestFocus();
+            return;
+        }
+        else if(pwd.isEmpty()){
+            loginPwd.setError("Enter Password");
+            loginPwd.requestFocus();
+            return;
+        }
+        else{
+            loginProgressBar.setVisibility(View.VISIBLE);
+            mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+//                        finish(); // cannot come back to login page by clicking back button
+                        loginProgressBar.setVisibility(View.GONE);
+                        Intent intent = new Intent(com.example.activitytracker.LoginActivity.this, WelcomeActivity.class);
+//                        //when clicked back after logging in, page should not come out of the app.so using addFlags method
+//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                    else{
+                        loginProgressBar.setVisibility(View.GONE);
+                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    //checking if the user is already logged in - then send him in directly.
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        if(mAuth.getCurrentUser()!=null){
+//            finish();
+//            startActivity(new Intent(this, ProfileActivity.class ));
+//        }
+//    }
+
+    public void loginOnSignUpClick(View view){
+        Intent intent = new Intent(com.example.activitytracker.LoginActivity.this, RegisterUserActivity.class);
         startActivity(intent);
     }
 
-    public void onLoginBtnClick(View view){
-        EditText username = findViewById(R.id.username);
-        EditText password = findViewById(R.id.password);
-        String user = username.getText().toString();
-        invalidCount++;
-
-        if(user.equals("rbala1")){ //USERNAME - should be matched to the db value
-            if (password.getText().toString().equals("raghul123")) { // PASSWORD - should be matched to the db value
-                String text = "Redirecting... to ACT main screen";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(this, text, duration);
-                toast.show();
-                Intent intent = new Intent(com.example.activitytracker.LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        }
-        else{
-            if(invalidCount == 1) {
-                String text = "Please check your username or password.\n" +  " Number of attempts left: 2";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(this, text, duration);
-                toast.show();
-            }
-            else if(invalidCount == 2){
-                String text = "Please check your username or password.\n" +  "Number of attempts left: 1";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(this, text, duration);
-                toast.show();
-            }
-            else{
-                String text = "Limit exceeded. Restart app to continue.";
-                Button button = findViewById(R.id.loginBtn);
-                int duration = Toast.LENGTH_SHORT;
-                //disabling the button so that when limit is exceeded user cannot login.
-                button.setEnabled(false);
-                Toast toast = Toast.makeText(this, text, duration);
-                toast.show();
-            }
-        }
-    }
 }
